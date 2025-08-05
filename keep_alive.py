7c2a555486ed5c139738a9c362e30dfe27ec4d78
RabiatulSyafiqah <rabiatulsyafiqahhh@gmail.com>
2025-08-05 10:04:56 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -2,7 +2,7 @@ from flask import Flask
 from threading import Thread
 import os
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
@@ -15,3 +15,4 @@ def run():
 def keep_alive():
     t = Thread(target=run)
     t.start()
+
